4553265d19f82e5e310c5888785a25038d4fcf96
Scott Madden <smadden5@gmail.com>
2022-03-30 13:28:42 -0500

    Articulate_me

---
+++ users/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email')
+
+# class LoginForm(forms.Form):
+#     username = forms.CharField(label = "User Name")
+#     password = forms.CharField(label = "Password",widget = forms.PasswordInput)
+
+# class RegisterForm(forms.Form):
+#     username = forms.CharField(max_length = 50,label = "User Name")
+#     password = forms.CharField(max_length=20,label = "Password",widget = forms.PasswordInput)
+#     confirm = forms.CharField(max_length=20,label ="Confirm Password",widget = forms.PasswordInput)
+    
+#     def clean(self):
+#         username = self.cleaned_data.get("username")
+#         password = self.cleaned_data.get("password")
+#         confirm = self.cleaned_data.get("confirm")
+
+#         if password and confirm and password != confirm:
+#             raise forms.ValidationError("Passwords Do Not Match")
+
+#         values = {
+#             "username" : username,
+#             "password" : password
+#         }
+#         return values
---
+++ users/views.py
@@ -0,0 +1,58 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .forms import CustomUserCreationForm
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+# def register(request):
+
+#     form = RegisterForm(request.POST or None)
+#     if form.is_valid():
+#         username = form.cleaned_data.get("username")
+#         password = form.cleaned_data.get("password")
+
+#         newUser = User(username =username)
+#         newUser.set_password(password)
+
+#         newUser.save()
+#         login(request,newUser)
+#         messages.info(request,"You Have Successfully Registered...")
+
+#         return redirect("index")
+#     context = {
+#             "form" : form
+#         }
+#     return render(request,"register.html",context)
+
+    
+    
+# def loginUser(request):
+#     form = LoginForm(request.POST or None)
+
+#     context = {
+#         "form":form
+#     }
+
+#     if form.is_valid():
+#         username = form.cleaned_data.get("username")
+#         password = form.cleaned_data.get("password")
+
+#         user = authenticate(username = username,password = password)
+
+#         if user is None:
+#             messages.info(request,"Username or Password Incorrect")
+#             return render(request,"login.html",context)
+
+#         messages.success(request,"You have Successfully Logged In")
+#         login(request,user)
+#         return redirect("index")
+#     return render(request,"login.html",context)
+
+# def logoutUser(request):
+#     logout(request)
+#     messages.success(request,"You have Successfully Logged Out")
+#     return redirect("index")
